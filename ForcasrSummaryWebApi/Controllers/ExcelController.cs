0a988badf44fd816702756f8269a4ed9035a7c57
Safiya <safiyashaikhjc@gmail.com>
2023-04-17 12:45:23 +0530

    added current year extra fields and tring to add quaters data for category field

--- ForcasrSummaryWebApi/Controllers/ExcelController.cs
+++ ForcasrSummaryWebApi/Controllers/ExcelController.cs
@@ -10,6 +10,7 @@ namespace ForcasrSummaryWebApi.Controllers
     public class ExcelController : ControllerBase
     {
         public static string _headers = "Total,C1,C2,C3,C4,C5,C6,C7,C8,C9,C10,C11,C12,C13";
+        public static string _quaterHeaders = "FY MAT,H1,H2, Q1,Q2,Q3,Q4";
         private readonly budgetForecastContext? _context;
         public ExcelController(budgetForecastContext? context)
         {
@@ -62,12 +63,45 @@ namespace ForcasrSummaryWebApi.Controllers
                     summaryDataArray.Add(nestedvalues);
                 }
 
-                var additionalValues = new[] { yearData.First().Year.ToString(), "UL $ Share", $"= SUM(C{categoryColumnNo + 1}: C{categoryColumnNo + 1})", $"= SUM(D{categoryColumnNo}: D{categoryColumnNo + 1})", $"= SUM(F{categoryColumnNo}: F{categoryColumnNo + 1})", $"= SUM(G{categoryColumnNo}: G{categoryColumnNo + 1})", $"= SUM(H{categoryColumnNo}: H{categoryColumnNo + 1})", $"= SUM(H{categoryColumnNo}: H{categoryColumnNo + 1})", $"= SUM(I{categoryColumnNo}: I{categoryColumnNo + 1})", $"= SUM(J{categoryColumnNo}: J{categoryColumnNo + 1})", $"= SUM(K{categoryColumnNo}: K{categoryColumnNo + 1})", $"= SUM(L{categoryColumnNo}: L{categoryColumnNo + 1})", $"= SUM(M{categoryColumnNo}: M{categoryColumnNo + 1})", $"= SUM(N{categoryColumnNo}: N{categoryColumnNo + 1})", $"= SUM(O{categoryColumnNo}: O{categoryColumnNo + 1})" };
-                summaryDataArray.Add(additionalValues);
-                if (dataByYear.Last().Key != yearData.Key)
+                // var additionalValues= Array.Empty<string>();
+                if (dataByYear.Last().Key == yearData.Key)
                 {
                     //need to implement
+                    var additionalValues = new[] { yearData.First().Year.ToString(), "$ % Chg", $"=C{categoryColumnNo + 1}/ C{categoryColumnNo - 3}-1", $"=D{categoryColumnNo + 1}/ D{categoryColumnNo - 3}-1", $"=E{categoryColumnNo + 1}/ E{categoryColumnNo - 3}-1", $"=F{categoryColumnNo + 1}/ F{categoryColumnNo - 3}-1", $"=G{categoryColumnNo + 1}/ G{categoryColumnNo - 3}-1", $"=H{categoryColumnNo + 1}/ H{categoryColumnNo - 3}-1", $"=I{categoryColumnNo + 1}/ I{categoryColumnNo - 3}-1", $"=J{categoryColumnNo + 1}/ J{categoryColumnNo - 3}-1", $"=K{categoryColumnNo + 1}/ K{categoryColumnNo - 3}-1", $"=L{categoryColumnNo + 1}/ L{categoryColumnNo - 3}-1", $"=M{categoryColumnNo + 1}/ M{categoryColumnNo - 3}-1", $"=N{categoryColumnNo + 1}/ N{categoryColumnNo - 3}-1", $"=O{categoryColumnNo + 1}/ O{categoryColumnNo - 3}-1" };
+                    var additionalValues1 = new[] { yearData.First().Year.ToString(), "UL $ Share", $"=C{categoryColumnNo + 1}/ C{categoryColumnNo}*100", $"=D{categoryColumnNo + 1}/ D{categoryColumnNo}*100", $"=E{categoryColumnNo + 1}/ E{categoryColumnNo}*100", $"=F{categoryColumnNo + 1}/ F{categoryColumnNo}*100", $"=G{categoryColumnNo + 1}/ G{categoryColumnNo}*100", $"=H{categoryColumnNo + 1}/ H{categoryColumnNo}*100", $"=I{categoryColumnNo + 1}/ I{categoryColumnNo}*100", $"=J{categoryColumnNo + 1}/ J{categoryColumnNo}*100", $"=K{categoryColumnNo + 1}/ K{categoryColumnNo}*100", $"=L{categoryColumnNo + 1}/ L{categoryColumnNo}*100", $"=M{categoryColumnNo + 1}/ M{categoryColumnNo}*100", $"=N{categoryColumnNo + 1}/ N{categoryColumnNo}*100", $"=O{categoryColumnNo + 1}/ O{categoryColumnNo}*100" };
+                    var additionalValues2 = new[] { yearData.First().Year.ToString(), "UL BPS Chg", $"=(C{categoryColumnNo + 3}- C{categoryColumnNo - 2})*100", $"=(D{categoryColumnNo + 3}- D{categoryColumnNo - 2})*100", $"=(E{categoryColumnNo + 3}- E{categoryColumnNo - 2})*100", $"=(F{categoryColumnNo + 3}- F{categoryColumnNo - 2})*100", $"=(G{categoryColumnNo + 3}- G{categoryColumnNo - 2})*100", $"=(H{categoryColumnNo + 3}- H{categoryColumnNo - 2})*100", $"=(I{categoryColumnNo + 3}- I{categoryColumnNo - 2})*100", $"=(J{categoryColumnNo + 3}- J{categoryColumnNo - 2})*100", $"=(K{categoryColumnNo + 3}- K{categoryColumnNo - 2})*100", $"=(L{categoryColumnNo + 3}- L{categoryColumnNo - 2})*100", $"=(M{categoryColumnNo + 3}- M{categoryColumnNo - 2})*100", $"=(N{categoryColumnNo + 3}- N{categoryColumnNo - 2})*100", $"=(O{categoryColumnNo + 3}- 0{categoryColumnNo - 2})*100" };
+                    var additionalValues3 = new[] { yearData.First().Year.ToString(), "Proj Sales UL", $"=C{categoryColumnNo + 1}", $"=D{categoryColumnNo + 1}", $"=E{categoryColumnNo + 1}", $"=F{categoryColumnNo + 1}", $"=G{categoryColumnNo + 1}", $"=H{categoryColumnNo + 1}", $"=I{categoryColumnNo + 1}", $"=J{categoryColumnNo + 1}", $"=K{categoryColumnNo + 1}", $"=L{categoryColumnNo + 1}", $"=M{categoryColumnNo + 1}", $"=N{categoryColumnNo + 1}", $"=O{categoryColumnNo + 1}" };
+                    var additionalValues4 = new[] { yearData.First().Year.ToString(), "Proj $ % Chg", $"=C{categoryColumnNo + 5}/ C{categoryColumnNo - 3}-1", $"=D{categoryColumnNo + 5}/ D{categoryColumnNo - 3}-1", $"=E{categoryColumnNo + 5}/ E{categoryColumnNo - 3}-1", $"=F{categoryColumnNo + 5}/ F{categoryColumnNo - 3}-1", $"=G{categoryColumnNo + 5}/ G{categoryColumnNo - 3}-1", $"=H{categoryColumnNo + 5}/ H{categoryColumnNo - 3}-1", $"=I{categoryColumnNo + 5}/ I{categoryColumnNo - 3}-1", $"=J{categoryColumnNo + 5}/ J{categoryColumnNo - 3}-1", $"=K{categoryColumnNo + 5}/ K{categoryColumnNo - 3}-1", $"=L{categoryColumnNo + 5}/ L{categoryColumnNo - 3}-1", $"=M{categoryColumnNo + 5}/ M{categoryColumnNo - 3}-1", $"=N{categoryColumnNo + 5}/ N{categoryColumnNo - 3}-1", $"=O{categoryColumnNo + 5}/ {categoryColumnNo - 3}-1" };
+                    var additionalValues5 = new[] { yearData.First().Year.ToString(), "Proj UL $ Share", $"=C{categoryColumnNo + 5}/ C{categoryColumnNo}*100", $"=D{categoryColumnNo + 5}/ D{categoryColumnNo}*100", $"=E{categoryColumnNo + 5}/ E{categoryColumnNo}*100", $"=F{categoryColumnNo + 5}/ F{categoryColumnNo}*100", $"=G{categoryColumnNo + 5}/ G{categoryColumnNo}*100", $"=H{categoryColumnNo + 5}/ H{categoryColumnNo}*100", $"=I{categoryColumnNo + 5}/ I{categoryColumnNo}*100", $"=J{categoryColumnNo + 5}/ J{categoryColumnNo}*100", $"=K{categoryColumnNo + 5}/ K{categoryColumnNo}*100", $"=L{categoryColumnNo + 5}/ L{categoryColumnNo}*100", $"=M{categoryColumnNo + 5}/ M{categoryColumnNo}*100", $"=N{categoryColumnNo + 5}/ N{categoryColumnNo}*100", $"=O{categoryColumnNo + 5}/ O{categoryColumnNo}*100" };
+                    var additionalValues6 = new[] { yearData.First().Year.ToString(), "Proj BPS Chg", $"=(C{categoryColumnNo + 7} - C{categoryColumnNo - 2})*100", $"=(D{categoryColumnNo + 7} - D{categoryColumnNo - 2})*100", $"=(E{categoryColumnNo + 7} - E{categoryColumnNo - 2})*100", $"=(F{categoryColumnNo + 7} - F{categoryColumnNo - 2})*100", $"=(G{categoryColumnNo + 7} - G{categoryColumnNo - 2})*100", $"=(H{categoryColumnNo + 7} - H{categoryColumnNo - 2})*100", $"=(I{categoryColumnNo + 7} - I{categoryColumnNo - 2})*100", $"=(J{categoryColumnNo + 7} - J{categoryColumnNo - 2})*100", $"=(K{categoryColumnNo + 7} - K{categoryColumnNo - 2})*100", $"=(L{categoryColumnNo + 7} - L{categoryColumnNo - 2})*100", $"=(M{categoryColumnNo + 7} - M{categoryColumnNo - 2})*100", $"=(N{categoryColumnNo + 7} - N{categoryColumnNo - 2})*100", $"=(O{categoryColumnNo + 7} - O{categoryColumnNo - 2})*100" };
+                    var additionalValues7 = new[] { yearData.First().Year.ToString(), "Proj Bps vs Forecast", "0.0", "0.0", $"=(E{categoryColumnNo + 8} - E{categoryColumnNo + 4})", $"=(F{categoryColumnNo + 8} - F{categoryColumnNo + 4})", $"=(G{categoryColumnNo + 8} - G{categoryColumnNo + 4})", $"=(H{categoryColumnNo + 8} - H{categoryColumnNo + 4})", $"=(I{categoryColumnNo + 8} - I{categoryColumnNo + 4})", $"=(J{categoryColumnNo + 8} - J{categoryColumnNo + 4})", $"=(K{categoryColumnNo + 8} - K{categoryColumnNo + 4})", $"=(L{categoryColumnNo + 8} - L{categoryColumnNo + 4})", $"=(M{categoryColumnNo + 8} - M{categoryColumnNo + 4})", $"=(N{categoryColumnNo + 8} - N{categoryColumnNo + 4})", $"=(O{categoryColumnNo + 8} - O{categoryColumnNo + 4})" };
+                    summaryDataArray.Add(additionalValues);
+                    summaryDataArray.Add(additionalValues1);
+                    summaryDataArray.Add(additionalValues2);
+                    summaryDataArray.Add(additionalValues3);
+                    summaryDataArray.Add(additionalValues4);
+                    summaryDataArray.Add(additionalValues5);
+                    summaryDataArray.Add(additionalValues6);
+                    summaryDataArray.Add(additionalValues7);
+                    Dictionary<string, int> mergeList1 = new Dictionary<string, int>
+                {
+                    { "row", categoryColumnNo -1 },
+                    { "col", 0 },
+                    { "rowspan",10 },
+                    { "colspan", 1 }
+                };
+
+                    mergeArray.Add(mergeList1);
+
                 }
+                else
+                {
+                    var additionalValues = new[] { yearData.First().Year.ToString(), "UL $ Share", $"=C{categoryColumnNo + 1}/ C{categoryColumnNo}*100", $"=D{categoryColumnNo + 1}/ D{categoryColumnNo}*100", $"=E{categoryColumnNo + 1}/ E{categoryColumnNo}*100", $"=F{categoryColumnNo + 1}/ F{categoryColumnNo}*100", $"=G{categoryColumnNo + 1}/ G{categoryColumnNo}*100", $"=H{categoryColumnNo + 1}/ H{categoryColumnNo}*100", $"=I{categoryColumnNo + 1}/ I{categoryColumnNo}*100", $"=J{categoryColumnNo + 1}/ J{categoryColumnNo}*100", $"=K{categoryColumnNo + 1}/ K{categoryColumnNo}*100", $"=L{categoryColumnNo + 1}/ L{categoryColumnNo}*100", $"=M{categoryColumnNo + 1}/ M{categoryColumnNo}*100", $"=N{categoryColumnNo + 1}/ N{categoryColumnNo}*100", $"=O{categoryColumnNo + 1}/ O{categoryColumnNo}*100" };
+                    summaryDataArray.Add(additionalValues);
+
+                }
+
+                //}
                 var nullarray = new[] { "" };
                 summaryDataArray.Add(nullarray);
 
@@ -80,6 +114,7 @@ namespace ForcasrSummaryWebApi.Controllers
                 };
 
                 mergeArray.Add(mergeList);
+               // bindingQuatersData(dataByYear, categoryColumnNo);
                 categoryColumnNo += 4;
 
             }
@@ -93,6 +128,34 @@ namespace ForcasrSummaryWebApi.Controllers
             return Ok(output);
         }
 
+
+        public string bindingQuatersData(IOrderedEnumerable<IGrouping<long?, USP_GetSummaryDataResult>> year, int categoryColumnNo)
+        {
+            var quaterHeaders = _quaterHeaders.Split(',');
+            var result = new string[quaterHeaders.Length + 1];
+            result[0] = "Body Cleansing";
+
+            for (var i = 1; i < result.Length; i++)
+            {
+                result[i] = quaterHeaders[i - 1];
+            }
+            var quaterDataArray = new List<string[]> { result };
+            var yearData = year;
+
+            foreach (var years in year)
+            {
+                if (years.First() == yearData)
+                {
+                    var values = new[] { "", "", "", "", "", "", "" };
+                    quaterDataArray.Add(values);
+                }
+                else
+                {
+                }
+
+            }
+            return "";
+        }
 
         public static string getColumnName(int columnNumber)
         {
